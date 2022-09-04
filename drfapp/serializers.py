3e7dd0ff2e328758932ec5d3dd7d1fb367a96988
sugam-hub <sgmpdl@gmail.com>
2022-09-04 16:26:42 +0545

    rest_framework

---
+++ drfapp/serializers.py
@@ -0,0 +1,13 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Student
+
+# Create serializer class
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = (
+            'name', 'age'
+        )
